bf123d8ccd505309b269ab602b155f2de8e2982c
Saadsnani <saad@example.com>
2026-02-04 03:19:46 +0100

    Fix: Clean up MainActivity and layout, add required permissions for Capacitor

--- android/app/src/main/java/com/solardashboard/app/MainActivity.java
+++ android/app/src/main/java/com/solardashboard/app/MainActivity.java
@@ -1,38 +1,21 @@
 package com.solardashboard.app;
 
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.widget.SearchView;
-
+import android.util.Log;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
-    private WebView webView;
-    private SearchView searchView;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // Capacitor automatically loads the web app
+        Log.d("MainActivity", "Application started successfully");
+    }
 
-        webView = findViewById(R.id.webview);
-        searchView = findViewById(R.id.search_view);
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                // Handle search query submission
-                // For example, you can load a URL in the WebView
-                webView.loadUrl("https://www.google.com/search?q=" + query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                // Handle search query text change
-                return false;
-            }
-        });
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "Activity started");
     }
 }
